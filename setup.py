b1b742f58255a1ff13a3a79e63dc53378f960beb
Melug <sw06d103@gmail.com>
2011-11-16 13:47:01 +0800

    added setuptools support

---
+++ setup.py
@@ -0,0 +1,23 @@
+import os
+from setuptools import setup
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+setup(
+    name = "Sudarmaa",
+    version = "0.1",
+    author = "Tulga",
+    author_email = "sw06d103@gmail.com",
+    description = ("Sudarmaa project"),
+    license = "BSD",
+    long_description=read('README'),
+    install_requires=[
+        'Django',
+        'pinax',
+        'django_debug_toolbar',
+        'django_compressor',
+        'django_staticfiles',
+        'PIL',
+    ],
+)
