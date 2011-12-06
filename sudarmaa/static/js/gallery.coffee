50dc41a6d4228144a357de8f104daccc48f435ac
melug <sw06d103@gmail.com>
2011-12-06 09:59:45 +0800

    go ahead, going good.

---
+++ sudarmaa/static/js/gallery.coffee
@@ -0,0 +1,2 @@
+display_picture_form = (url) ->
+    image_window = window.open url, 'Choose an image', 'menubar=1,width=400,height=400'
---
+++ sudarmaa/static/js/gallery.js
@@ -0,0 +1,6 @@
+var display_picture_form;
+
+display_picture_form = function(url) {
+  var image_window;
+  return image_window = window.open(url, 'Choose an image', 'menubar=1,width=400,height=400');
+};
