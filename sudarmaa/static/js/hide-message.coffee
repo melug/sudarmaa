7ef75581f6f2c8048a9546dcbf6188e686a1d5bb
melug <sw06d103@gmail.com>
2011-12-10 16:00:52 +0800

    added automatic "hide message"

---
+++ sudarmaa/static/js/hide-message.coffee
@@ -0,0 +1,3 @@
+$ ->
+    alert_message = $ 'div.alert-message'
+    alert_message.delay(5000).fadeOut(500)
---
+++ sudarmaa/static/js/hide-message.js
@@ -0,0 +1,6 @@
+
+$(function() {
+  var alert_message;
+  alert_message = $('div.alert-message');
+  return alert_message.delay(5000).fadeOut(500);
+});
